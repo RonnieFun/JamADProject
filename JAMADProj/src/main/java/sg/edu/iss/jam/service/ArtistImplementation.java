88715457327149ba192d61e3ba4470edfee04ce5
zhaoq <zhaoq@172.31.28.170>
2021-08-15 23:24:26 +0800

    ZQ: subscribe/unsubscribe an artist
    1. User model: changed the relationship with subscribers  to OneToMany;
    2. Subscribed model: chang the relationship with User to ManyToOne, add
    attribute targetId;
    3. add ZQController, add watchvideozq.html
    4. application.properties file: add "&createDatabaseIfNotExist=true" so
    that no need to create database manually
    5. Application file: add ZQ's dummy data
    6. layout.html: add scripts used for ajax call
    7. Roles model: add a constructor used for initializing data
    8. Service: add methods in Artist service and User service

--- JAMADProj/src/main/java/sg/edu/iss/jam/service/ArtistImplementation.java
+++ JAMADProj/src/main/java/sg/edu/iss/jam/service/ArtistImplementation.java
@@ -1,8 +1,48 @@
 package sg.edu.iss.jam.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import sg.edu.iss.jam.model.Subscribed;
+import sg.edu.iss.jam.model.User;
+import sg.edu.iss.jam.repo.SubscribedRepository;
+import sg.edu.iss.jam.repo.UserRepository;
+
 @Service
-public class ArtistImplementation implements UserInterface {
+public class ArtistImplementation implements ArtistInterface {
+	
+	@Autowired
+	UserRepository urepo;
+	
+	@Autowired
+	SubscribedRepository subrepo;
+	
+	@Transactional
+	public User findById(Long userID) {
+		
+		return urepo.findById(userID).get();
+	}
+
+
+	@Transactional
+	public User saveUser(User user) {
+		// TODO Auto-generated method stub
+		return urepo.save(user);
+	}
+	
+	
+	@Transactional
+	public Subscribed saveSubscribed(Subscribed subscribed) {
+		// TODO Auto-generated method stub
+		return subrepo.save(subscribed);
+	}
+
+
+	@Transactional
+	public void deleteSubscribed(Subscribed s) {
+		subrepo.delete(s);
+	}
 
 }
--- JAMADProj/src/main/java/sg/edu/iss/jam/service/UserImplementation.java
+++ JAMADProj/src/main/java/sg/edu/iss/jam/service/UserImplementation.java
@@ -1,8 +1,56 @@
 package sg.edu.iss.jam.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import sg.edu.iss.jam.model.Playlists;
+import sg.edu.iss.jam.model.Subscribed;
+import sg.edu.iss.jam.model.User;
+import sg.edu.iss.jam.repo.MediaRepository;
+import sg.edu.iss.jam.repo.SubscribedRepository;
+import sg.edu.iss.jam.repo.UserRepository;
 
 @Service
 public class UserImplementation implements UserInterface {
+
+	@Autowired
+	UserRepository urepo;
+
+	
+	@Autowired
+	MediaRepository mediarepo;
+	
+	@Autowired
+	SubscribedRepository subrepo;
+	
+	@Transactional
+	public User findById(Long userID) {
+		
+		return urepo.findById(userID).get();
+	}
+
+	@Transactional
+	public User saveUser(User user) {
+		
+		return urepo.save(user);
+	}
+
+	
+
+	@Transactional
+	public Subscribed saveSubscribed(Subscribed subscribed) {
+		// TODO Auto-generated method stub
+		return subrepo.save(subscribed);
+	}
+
+	@Transactional
+	public void deleteSubscribed(Subscribed s) {
+		
+		subrepo.delete(s);
+	}
 
 }
