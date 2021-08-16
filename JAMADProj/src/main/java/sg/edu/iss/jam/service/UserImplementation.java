8c7290cab479144d1a24e91a97ddd22acaa6a770
Max <wenquan624@gmail.com>
2021-08-16 15:06:08 +0800

    Merged from master

--- JAMADProj/src/main/java/sg/edu/iss/jam/service/UserImplementation.java
+++ JAMADProj/src/main/java/sg/edu/iss/jam/service/UserImplementation.java
@@ -7,19 +7,18 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
 import sg.edu.iss.jam.model.Media;
 import sg.edu.iss.jam.model.Playlists;
 import sg.edu.iss.jam.model.User;
 import sg.edu.iss.jam.repo.MediaRepository;
 import sg.edu.iss.jam.repo.PlaylistsRepository;
-=======
+
 import sg.edu.iss.jam.model.Playlists;
 import sg.edu.iss.jam.model.Subscribed;
 import sg.edu.iss.jam.model.User;
 import sg.edu.iss.jam.repo.MediaRepository;
 import sg.edu.iss.jam.repo.SubscribedRepository;
->>>>>>> refs/heads/master
+
 import sg.edu.iss.jam.repo.UserRepository;
 
 @Service
@@ -27,13 +26,15 @@ public class UserImplementation implements UserInterface {
 
 	@Autowired
 	UserRepository urepo;
-<<<<<<< HEAD
-	
+
 	@Autowired
 	PlaylistsRepository plrepo;
 	
 	@Autowired
 	MediaRepository mediarepo;
+	
+	@Autowired
+	SubscribedRepository subrepo;
 	
 	@Transactional
 	public User findById(Long userID) {
@@ -89,35 +90,9 @@ public class UserImplementation implements UserInterface {
 		return plrepo.saveAll(playlists);
 	}
 
-	
-
-	
-=======
-
-	
-	@Autowired
-	MediaRepository mediarepo;
-	
-	@Autowired
-	SubscribedRepository subrepo;
-	
-	@Transactional
-	public User findById(Long userID) {
-		
-		return urepo.findById(userID).get();
-	}
-
-	@Transactional
-	public User saveUser(User user) {
-		
-		return urepo.save(user);
-	}
-
-	
-
 	@Transactional
 	public Subscribed saveSubscribed(Subscribed subscribed) {
-		// TODO Auto-generated method stub
+		
 		return subrepo.save(subscribed);
 	}
 
@@ -126,6 +101,5 @@ public class UserImplementation implements UserInterface {
 		
 		subrepo.delete(s);
 	}
->>>>>>> refs/heads/master
 
 }
--- JAMADProj/src/main/java/sg/edu/iss/jam/service/UserInterface.java
+++ JAMADProj/src/main/java/sg/edu/iss/jam/service/UserInterface.java
@@ -2,15 +2,12 @@ package sg.edu.iss.jam.service;
 
 import java.util.List;
 
-<<<<<<< HEAD
 import sg.edu.iss.jam.model.Media;
 import sg.edu.iss.jam.model.Playlists;
-=======
-import javax.print.attribute.standard.Media;
 
 import sg.edu.iss.jam.model.Playlists;
 import sg.edu.iss.jam.model.Subscribed;
->>>>>>> refs/heads/master
+
 import sg.edu.iss.jam.model.User;
 
 public interface UserInterface {
@@ -19,7 +16,7 @@ public interface UserInterface {
 	
 	User saveUser(User user);
 	
-<<<<<<< HEAD
+
 	List<Playlists> findPlaylistsByUserId(Long userID);
 	
 	Playlists findPlaylistByPlaylistID(long playlistID);
@@ -33,11 +30,11 @@ public interface UserInterface {
 	void deleteByid(Long ID);
 	
 	List<Media> findMediaListByPlayListID(Long playlistID);
-=======
+
 	
 	Subscribed saveSubscribed(Subscribed subscribed);
 
 	void deleteSubscribed(Subscribed s);
->>>>>>> refs/heads/master
+
 }
 
