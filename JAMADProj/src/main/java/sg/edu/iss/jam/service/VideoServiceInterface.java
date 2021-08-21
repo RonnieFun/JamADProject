c8e351bc9b94b51e1989a8e839bd50c433ba3c74
Changying <Changying@DESKTOP-1141R4H>
2021-08-21 15:32:43 +0800

    scy changes
    1.add music/video landing page
    2.change media controller, video service, video service implementation,
    media repo

--- JAMADProj/src/main/java/sg/edu/iss/jam/service/VideoServiceInterface.java
+++ JAMADProj/src/main/java/sg/edu/iss/jam/service/VideoServiceInterface.java
@@ -1,10 +1,21 @@
 package sg.edu.iss.jam.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+
+import sg.edu.iss.jam.model.Media;
+import sg.edu.iss.jam.model.MediaType;
 
 public interface VideoServiceInterface {
 	
 	public ResponseEntity<byte[]> getContent(String location, String fileName, String range, String contentTypePrefix);
+	 
+	//scy-part
+	public List<Media> getMediaByTypeAndCount(MediaType mediaType);
+
+	public List<Media> getMediaByUserHistory(MediaType mediaType,LocalDate lesscurrentdate);
 
 
 }
